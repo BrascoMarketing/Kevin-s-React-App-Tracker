a68a75c4e38840eb2dd9ae177c5f5092fb948b7c
Kevin Adams <kevina@brasco.marketing>
2025-05-13 16:25:32 -0500

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Update this to match your GitHub repo name
 export default defineConfig({
+  base: '/Kevin-s-React-App-Tracker/',
   plugins: [react()],
-})
+});
